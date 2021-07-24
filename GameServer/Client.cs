23358e15b4fd9beeb2e2a4bb652204d357fe3255
achuq <achuq@DESKTOP-OVAC3KQ>
2021-07-24 17:53:13 +0530

    Add project files.

---
+++ GameServer/Client.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+	/// <summary>
+	/// Class to store all client information and communication protocol
+	/// </summary>
+	class Client
+	{
+		public static readonly int dataBufferSize = 4096;
+		public int id;
+		public TCP tcp;
+
+		public Client(int clientId)
+		{
+			id = clientId;
+			tcp = new TCP(id);
+		}
+
+		/// <summary>
+		/// TCP socket and communication API.
+		/// </summary>
+		public class TCP
+		{
+			public TcpClient socket;
+
+			private readonly int id;
+			private NetworkStream stream;
+			private byte[] receiveBuffer;
+
+			public TCP(int _id)
+			{
+				id = _id;
+			}
+
+			/// <summary>
+			/// Client's TCP connect method for tcp communication.
+			/// </summary>
+			/// <param name="_socket">The <see cref="TcpClient"/> to pass in</param>
+			public void Connect(TcpClient _socket)
+			{
+				socket = _socket;
+				socket.ReceiveBufferSize = dataBufferSize;
+
+				stream = socket.GetStream();
+
+				receiveBuffer = new byte[dataBufferSize];
+
+				stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+				// TODO: Send welcome packet.
+			}
+
+			private void ReceiveCallback(IAsyncResult result)
+			{
+				try
+				{
+					int byteLength = stream.EndRead(result);
+					if (byteLength <= 0)
+					{
+						// TODO: Disconnect
+						return;
+					}
+
+					byte[] data = new byte[byteLength];
+					Array.Copy(receiveBuffer, data, byteLength);
+
+					// TODO: Handle data.
+					stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error receiving TCP Data: {ex}");
+					// TODO: Disconnect.
+				}
+			}
+		}
+	}
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+	/// <summary>
+	/// Class which houses all the server logic
+	/// </summary>
+	class Server
+	{
+		public static int MaxPlayers { get; private set; }
+		public static int Port { get; private set; }
+		public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+		private static TcpListener tcpListener;
+
+		/// <summary>
+		/// Method to start the server
+		/// </summary>
+		/// <param name="maxPlayers">The maximum no of concurrent players</param>
+		/// <param name="port">The port to start the server listening on</param>
+		public static void Start(int maxPlayers, int port)
+		{
+			MaxPlayers = maxPlayers;
+			Port = port;
+
+			Console.WriteLine("Starting Server...");
+			InitializeServerData();
+
+			tcpListener = new TcpListener(IPAddress.Any, Port);
+			tcpListener.Start();
+			tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+			Console.WriteLine($"Server started on port {Port}.");
+		}
+
+		private static void TCPConnectCallback(IAsyncResult result)
+		{
+			TcpClient _Client = tcpListener.EndAcceptTcpClient(result);
+			tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+			Console.WriteLine($"Incoming connection from {_Client.Client.RemoteEndPoint}...");
+
+			for (int i = 1; i <= MaxPlayers; i++)
+			{
+				if (clients[i].tcp.socket == null)
+				{
+					clients[i].tcp.Connect(_Client);
+					return;
+				}
+			}
+
+			Console.WriteLine($"{_Client.Client.RemoteEndPoint} failed to connect: Server full!");
+		}
+
+		private static void InitializeServerData()
+		{
+			for (int i = 1; i <= MaxPlayers; i++)
+			{
+				clients.Add(i, new Client(i));	
+			}
+		}
+	}
+}
